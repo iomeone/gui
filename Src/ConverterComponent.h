5785554313eec07c158502aa4f2b2da9e0c560d1
iomeone <iomeonee@gmail.com>
2020-07-10 17:02:53 +0800

    string converter work!

--- Src/ConverterComponent.h
+++ Src/ConverterComponent.h
@@ -23,8 +23,21 @@ namespace HelloWorld
 			if (targetType == TypeOf<Noesis::String>() && Boxing::CanUnbox<Noesis::String>(value))
 			{
 				Noesis::String text = Boxing::Unbox<Noesis::String>(value);
-				text.Insert(0, "1111");
-				result = Boxing::Box<Noesis::String>(text);
+				Noesis::String rs;
+				rs += "char  " + text + "[]= {";
+				for (uint32_t i = 0; i < text.Size(); i++)
+				{
+					rs +=  "\'"  ;// +String(char(i)) + "\', ";
+					rs += text[i];
+					rs += "\'";
+					if(i != text.Size()-1)
+						rs += ", ";
+
+				}
+				rs += ", 0};";
+				
+				
+				result = Boxing::Box<Noesis::String>(rs);
 				return true;
 			}
 
